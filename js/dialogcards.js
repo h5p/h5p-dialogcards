d502f9feda7fe9a6980827407f7a984ff09a6896
Frode Petterson <frode.petterson@amendor.com>
2014-02-28 10:52:04 +0100

    HFJ-186: Center text and automatically left align text that uses more than a single line. Added try again button. Minor style fixes. Fixed card animation. Documented functions. Updated translations.

--- js/dialogcards.js
+++ js/dialogcards.js
@@ -12,70 +12,94 @@ H5P.Dialogcards = (function ($) {
    *
    * @param {Object} params Behavior settings
    * @param {Number} id Content identification
-   * @returns {_L8.C}
+   * @returns {C} self
    */
   function C(params, id) {
-    this.id = id;
+    var self = this;
+    
+    self.$ = $(self);
+    self.id = id;
 
     // Set default behavior.
-    this.params = $.extend({
+    self.params = $.extend({
       title: "Dialogue",
       description: "Sit in pairs and make up sentences where you include the expressions below.<br/>Example: I should have said yes, HOWEVER I kept my mouth shut.",
       next: "Next",
       prev: "Previous",
+      retry: "Try again",
       answer: "Turn",
       progressText: "Card @card of @total",
       endComment: "This was the last card. Press Try again to start over.",
       postUserStatistics: (H5P.postUserStatistics === true)
     }, params);
     
-    this._current = -1;
-    this._turned = [];
+    self._current = -1;
+    self._turned = [];
   };
 
   /**
-   * Append field to wrapper.
+   * Attach h5p inside the given container.
    *
    * @param {jQuery} $container
    */
   C.prototype.attach = function ($container) {
     var self = this;
     
-    this._$inner = $container.addClass('h5p-dialogcards').html('\
-      <div class="h5p-title">' + this.params.title + '</div>\
-      <div class="h5p-description">' + this.params.description + '</div>\
-     ' + C.createCards(this.params.dialogs, this.params.answer) + '\
+    self._$inner = $container.addClass('h5p-dialogcards').html('\
+      <div class="h5p-title">' + self.params.title + '</div>\
+      <div class="h5p-description">' + self.params.description + '</div>\
+     ' + C.createCards(self.params.dialogs, self.params.answer) + '\
       <div class="h5p-inner">\
-        <div class="h5p-button h5p-prev" role="button" tabindex="1">' + this.params.prev + '</div>\
-        <div class="h5p-button h5p-next" role="button" tabindex="1">' + this.params.next + '</div>\
         <div class="h5p-progress"></div>\
+        <div class="h5p-button h5p-prev" role="button" tabindex="1">' + self.params.prev + '</div>\
+        <div class="h5p-button h5p-next" role="button" tabindex="1">' + self.params.next + '</div>\
+        <div class="h5p-button h5p-retry h5p-disabled" role="button" tabindex="1">' + self.params.retry + '</div>\
       </div>');
+      
+    self._$inner.find('.h5p-card').each(function () {
+      self.alignText($(this));
+    });
     
-    this._$progress = this._$inner.find('.h5p-progress');
-    this._$current = this._$inner.find('.h5p-current');
+    self._$progress = self._$inner.find('.h5p-progress');
+    self._$current = self._$inner.find('.h5p-current');
     
-    this._$inner.find('.h5p-turn').click(function () {
+    self._$inner.find('.h5p-turn').click(function () {
       self.turnCard($(this).parent().parent());
     });
     
-    this._$prev = this._$inner.find('.h5p-prev').click(function () {
+    self._$prev = self._$inner.find('.h5p-prev').click(function () {
       self.prevCard();
     });
     
-    this._$next = this._$inner.find('.h5p-next').click(function () {
+    self._$next = self._$inner.find('.h5p-next').click(function () {
       self.nextCard();
     });
     
-    this.updateNavigation();
+    self._$retry = self._$inner.find('.h5p-retry').click(function () {
+      self.$.trigger('reset');
+    });
+    
+    self.updateNavigation();
+    
+    self.$.on('reset', function () {
+      self.reset();
+    });
   };
   
+  /**
+   * Creates html for all the cards.
+   * Uses a table to center text vertically.
+   *
+   * @param {Array} cards
+   * @param {String} turn button text
+   */
   C.createCards = function (cards, turn) {
     var html = '';
     for (var i = 0; i < cards.length; i++) {
       html += '\
         <div class="h5p-cardwrap' + (i === 0 ? ' h5p-current' : '') + '">\
           <div class="h5p-cardholder">\
-            <div class="h5p-card">' + cards[i].text + '</div>\
+            <div class="h5p-card"><table><tr><td>' + cards[i].text + '</td></tr></table></div>\
             <div class="h5p-button h5p-turn" role="button">' + turn + '</div>\
           </div>\
         </div>';
@@ -83,54 +107,137 @@ H5P.Dialogcards = (function ($) {
     return html;
   };
   
+  /**
+   * Update navigation text and show or hide buttons.
+   */
   C.prototype.updateNavigation = function () {
-    if (this._$current.next('.h5p-cardwrap').length) {
-      this._$next.removeClass('h5p-disabled');
+    var self = this;
+    
+    if (self._$current.next('.h5p-cardwrap').length) {
+      self._$next.removeClass('h5p-disabled');
+      self._$retry.addClass('h5p-disabled');
     }
     else {
-      this._$next.addClass('h5p-disabled');
+      self._$next.addClass('h5p-disabled');
+      if (self._$current.find('.h5p-turn').hasClass('h5p-disabled')) {
+        self._$retry.removeClass('h5p-disabled');
+      }
     }
     
-    if (this._$current.prev('.h5p-cardwrap').length) {
-      this._$prev.removeClass('h5p-disabled');
+    if (self._$current.prev('.h5p-cardwrap').length) {
+      self._$prev.removeClass('h5p-disabled');
     }
     else {
-      this._$prev.addClass('h5p-disabled');
+      self._$prev.addClass('h5p-disabled');
     }
     
-    this._$progress.text(this.params.progressText.replace('@card', this._$current.index() - 1).replace('@total', this.params.dialogs.length));
+    self._$progress.text(self.params.progressText.replace('@card', self._$current.index() - 1).replace('@total', self.params.dialogs.length));
   };
   
+  /**
+   * Show next card.
+   */
   C.prototype.nextCard = function () {
-    var $next = this._$current.next('.h5p-cardwrap');
+    var self = this;
+    var $next = self._$current.next('.h5p-cardwrap');
+    
     if ($next.length) {
-      this._$current.removeClass('h5p-current').addClass('h5p-previous');
-      this._$current = $next.addClass('h5p-current');
-      this.updateNavigation();
+      self._$current.removeClass('h5p-current').addClass('h5p-previous');
+      self._$current = $next.addClass('h5p-current');
+      self.updateNavigation();
     }
   };
   
+  /**
+   * Show previous card.
+   */
   C.prototype.prevCard = function () {
-    var $prev = this._$current.prev('.h5p-cardwrap');
-    if ($prev.length) {
-      this._$current.removeClass('h5p-current');
-      this._$current = $prev.addClass('h5p-current').removeClass('h5p-previous');
-      this.updateNavigation();
-    }
-  };
+    var self = this;
+    var $prev = self._$current.prev('.h5p-cardwrap');
     
-  C.prototype.turnCard = function ($card) {
-    $card.find('.h5p-card').text(this.params.dialogs[$card.index() - 2].answer);
-    $card.find('.h5p-card').addClass('h5p-collapse');
-    setTimeout(function () {
-        $card.find('.h5p-card').removeClass('h5p-collapse');
-      }, 150);
-    $card.find('.h5p-turn').addClass('h5p-disabled');
-    if (!this._$current.next('.h5p-cardwrap').length) {
-      $card.find('.h5p-cardholder').append('<div class="h5p-endcomment">' + this.params.endComment + '</div>');
+    if ($prev.length) {
+      self._$current.removeClass('h5p-current');
+      self._$current = $prev.addClass('h5p-current').removeClass('h5p-previous');
+      self.updateNavigation();
     }
   };
+  
+  /**
+   * Show the opposite site of the card.
+   *
+   * @param {jQuery} $card
+   */
+  C.prototype.turnCard = function ($card) {
+    var self = this;
+    var $c = $card.find('.h5p-card').addClass('h5p-collapse');
+
+    setTimeout(function () {
+      $c.removeClass('h5p-collapse');
+      self.alignText($c, self.params.dialogs[$card.index() - 2].answer);
+    }, 150);
+      
+    $card.find('.h5p-turn').addClass('h5p-disabled');
+    if (!self._$current.next('.h5p-cardwrap').length) {
+      $card.find('.h5p-cardholder').append('<div class="h5p-endcomment">' + self.params.endComment + '</div>');
+      self._$retry.removeClass('h5p-disabled');
+    }
+  };
+  
+  /**
+   * Use a table to vertically align text in the middle.
+   * Alignes text to the left if it's multiple lines.
+   *
+   * @param {jQuery} $card
+   * @param {String} text
+   */
+  C.prototype.alignText = function ($card, text) {
+    var self = this;
+    var $t = $card.find('table');
+    var $td = $t.find('td');
+
+    if (self._oneLineHeight === undefined) {
+      if (text === undefined) {
+        text = $td.text();
+      }
+      $td.text('M'); // Test char.
+      self._oneLineHeight = $t.height();
+    }
+    
+    if (text !== undefined) {
+      $td.text(text);
+    }
+    $t.css('height', '');
+    if ($t.height() > self._oneLineHeight) {
+      $td.addClass('h5p-left');
+    }
+    else {
+      $td.removeClass('h5p-left');
+    }
+    
+    if (self._cardHeight === undefined) {
+      self._cardHeight = $t.parent().height();
+    }
+    $t.css('height', self._cardHeight + 'px');
+  };
+  
+  /**
+   * Reset the task so that the user can do it again.
+   */
+  C.prototype.reset = function () {
+    var self = this;
+    var $cards = self._$inner.find('.h5p-cardwrap');
+    
+    self._$current.removeClass('h5p-current');
+    self._$current = $cards.filter(':first').addClass('h5p-current');
+    self.updateNavigation();
+    
+    $cards.each(function (index) {
+      var $card = $(this).removeClass('h5p-previous');
+      self.alignText($card, self.params.dialogs[$card.index() - 2].text);
+    });
+    self._$inner.find('.h5p-turn').removeClass('h5p-disabled');      
+    self._$inner.find('.h5p-endcomment').remove();
+  }
   
   return C;
 })(H5P.jQuery);
-
