1663036b398465ae412358ce9aef73d53ef2fe6d
Oliver Tacke <o.tacke@posteo.de>
2018-11-02 12:42:29 +0100

    HFP-2340 Add functions for card management
    - Separate function for pile creation
    - Function for updating piles according to results
    - Separate functions for creating selections from piles
    - Function for shuffling selections (or arrays)
    - Reset function implemented
    - Fixed wrong return value in find function
    
    Also: Remove experimental functions not needed anymore

--- src/h5p-dialogcards-cardmanager.js
+++ src/h5p-dialogcards-cardmanager.js
@@ -6,62 +6,163 @@ class CardManager {
    * @constructor
    */
   constructor(params, contentId, callbacks) {
-    this.cardPiles = [];
-    this.selectionIds = [];
-
-    this.currentId = 0;
-
+    this.params = params;
     this.cardPool = new CardPool(params, contentId, callbacks);
+
+    this.reset();
 
     return this;
   }
 
+  /**
+   * Create a selection of card ids depending on mode.
+   *
+   * @return {object[]} Selection of card ids.
+   */
   createSelection() {
-    // Normal mode only right now
-    this.selectionIds = this.cardPool.getCardIds();
-    this.cardPiles.push(new CardPile(this.selectionIds));
+    let selectionIds = [];
 
-    return this.selectionIds;
+    switch (this.params.mode) {
+      case 'repetition':
+        // Repetition mode
+        selectionIds = this.createSelectionRepetition();
+        break;
+      default:
+        // Normal mode
+        selectionIds = this.cardPool.getCardIds();
+    }
+
+    return selectionIds;
   }
 
+  /**
+   * Create piles depending on mode.
+   */
+  createPiles() {
+    this.cardPiles = [];
+    const pool = this.cardPool.getCardIds();
+
+    switch (this.params.mode) {
+      case 'repetition':
+        // Repetition mode
+        for (let i = 0; i < this.params.maxProficiency; i++) {
+          if (i === 0) {
+            this.cardPiles.push(new CardPile(pool));
+          }
+          else {
+            this.cardPiles.push(new CardPile());
+          }
+        }
+        break;
+
+      case 'normal':
+        // Normal mode. One pile only with all cards
+        this.cardPiles.push(new CardPile(pool));
+    }
+  }
+
+  /**
+   * Update piles.
+   *
+   * Just one "rule" for now. Could be amended with more modes.
+   *
+   * @param {object[]} results Results.
+   * @param {number} results.cardId Card that result is reported for.
+   * @param {boolean} results.result Result for that card.
+   */
+  updatePiles(results) {
+    results.forEach(result => {
+      // Find card in pile
+      const pileId = this.find(result.cardId);
+      if (pileId === -1) {
+        return;
+      }
+
+      // Move card to next pile or first pile
+      let newPileId = (result.result === true) ? pileId + 1 : 0;
+      newPileId = Math.max(0, Math.min(newPileId, this.cardPiles.length - 1));
+      this.cardPiles[pileId].remove(result.cardId);
+      this.cardPiles[newPileId].add(result.cardId, 'bottom');
+    });
+
+    this.round++;
+  }
+
+  /**
+   * Create card selection for repetition mode.
+   *
+   * Draw all cards from first pile with cards, 1/2 of next, 1/3 of next, ...
+   * Don't draw cards from last pile
+   * Get cards from top - will be moved at the bottom of other pile according to results (LIFO)
+   *
+   * @return {object[]} Ids of selected cards from pool.
+   */
+  createSelectionRepetition() {
+    let selectionIds = [];
+
+    let firstPileWithCards = null;
+    for (let j = 0; j < this.cardPiles.length - 1; j++) {
+      const pileAmount = this.cardPiles[j].length();
+
+      // Skip empty piles
+      if (firstPileWithCards === null && pileAmount === 0) {
+        continue;
+      }
+
+      // Remember first pile with cards
+      if (firstPileWithCards === null) {
+        firstPileWithCards = j;
+      }
+
+      // Draw cards favoring those of piles with low id
+      const drawAmount = Math.ceil(pileAmount * 1 / (1 + j - firstPileWithCards));
+      const cardsDrawn = this.cardPiles[j].peek(0, drawAmount);
+      selectionIds = selectionIds.concat(...cardsDrawn);
+    }
+
+    // Shuffle selection
+    selectionIds = this.shuffle(selectionIds);
+
+    return selectionIds;
+  }
+
+  /**
+   * Shuffle items (not in place).
+   *
+   * @param {object[]} items Items.
+   * @return {object[]} Shuffled items.
+   */
+  shuffle(items) {
+    const copies = items.slice();
+    for (let i = copies.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [copies[i], copies[j]] = [copies[j], copies[i]];
+    }
+    return copies;
+  }
+
+  /**
+   * Find id of pile that contains card with particular id.
+   *
+   * @param {number} id Id of card to be found.
+   * @return {number} Id of pile or -1.
+   */
   find(id) {
     let found = -1;
     this.cardPiles.forEach((cardPile, index) => {
       if (found !== -1) {
-        return;
+        return found;
       }
       if (cardPile.contains(id))  {
         found = index;
       }
     });
-  }
-
-  hasNextCard() {
-    return this.currentId < this.selectionIds.length - 1;
-  }
-
-  hasPreviousCard() {
-    return this.currentId > 0;
-  }
-
-  getNextCard() {
-    if (!this.hasNextCard()) {
-      return;
-    }
-    this.currentId++;
-    return this.cardPool.getCard(this.selectionIds[this.currentId]);
-  }
-
-  getPreviousCard() {
-    if (!this.hasPreviousCard) {
-      return;
-    }
-    this.currentId--;
-    return this.cardPool.getCard(this.selectionIds[this.currentId]);
+    return found;
   }
 
   reset() {
-    return;
+    this.round = 1;
+    this.createPiles();
   }
 
   getCard(id) {
