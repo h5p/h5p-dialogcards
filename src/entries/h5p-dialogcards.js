c8a19d99a4a7c7e5c91b9e05a19c2fd66656c750
Oliver Tacke <o.tacke@posteo.de>
2018-11-05 09:21:34 +0100

    HFP-2340 Move scripts to separate folder

--- src/entries/h5p-dialogcards.js
+++ src/entries/h5p-dialogcards.js
@@ -1,6 +1,6 @@
 import "../styles/dialogcards.css";
-import Dialogcards from "../h5p-dialogcards.js";
+import Dialogcards from "../scripts/h5p-dialogcards.js";
 
 // Load library
-H5P = H5P || {};
+// H5P = H5P || {};
 H5P.Dialogcards = Dialogcards;
